ca9156b26b34769b69ec03697bb7ae7ad40729cb
Deot <zrd0921@qq.com>
2018-08-22 14:27:05 +0800

    add `key` param in `ModuleEdit`

--- src/components/Modules/ModulesEdit.js
+++ src/components/Modules/ModulesEdit.js
@@ -4,6 +4,7 @@ const ModulesEdit = (props) => {
 	const type = item ? item.split('#')[0] : null;
 	return modules[type] 
 		? createElement(modules[type].Editor, {
+			key: item, // 确保他的唯一性, 可能前端两个编辑的输入框内容会被保留
 			...props
 		})
 		: null;
